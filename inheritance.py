666da33f49f89a8d74dd4cec375b8a91b00d47c5
Yuvraj <shindeyuvi123@gmail.com>
2021-11-18 16:18:42 +0530

    first commit

---
+++ inheritance.py
@@ -0,0 +1,21 @@
+class Demo:
+    def DemoMethod(self):
+     print("Demo class method")
+
+class User:
+    def UserMethod(self):
+     print("User class method")
+
+class Info:
+     def InfoMethod(self):
+         print("Info class method ")
+
+
+d=Demo()
+d.DemoMethod()
+
+u=User()
+u.UserMethod()
+
+i=Info()
+i.InfoMethod()
